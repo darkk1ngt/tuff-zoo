f60491919ba64f2d992a0392dd1e3d4544dcd6e4
darkk1ngt <noellusale7@gmail.com>
2026-02-05 14:41:19 +0000

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,7 +36,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
